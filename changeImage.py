dd55338ad3123f62cf25b7f9f1216bfd26b1b8ea
Tomo <yokada@umich.edu>
2020-08-21 11:44:11 -0400

    Automating Real-World Tasks with Python Week 4 Qwiklabs Assessment: Automate updating catalog informa Working with supplier images

---
+++ changeImage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Size: Change image resolution from 3000x2000 to 600x400 pixel
+#Format: Change image format from .TIFF to .JPEG
+
+# TODO: Use convert("RGB") method for converting RGBA to RGB image.
+
+
+# TODO: After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
