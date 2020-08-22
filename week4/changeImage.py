77d58c468857a4c4bfe3895a3e2004b06edc1d6b
Tomo <yokada@umich.edu>
2020-08-22 15:36:23 -0400

    modify tiff to jpeg

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -2,8 +2,8 @@
 from PIL import Image
 import glob, os
 
-read_dir = "/supplier-data/images"
-save_dir = "/supplier-data/images"
+read_dir = "supplier-data/images"
+save_dir = "supplier-data/images"
 
 for file_name in glob.glob(os.path.join(read_dir,"*")):
     im = Image.open(file_name)
@@ -12,6 +12,7 @@ for file_name in glob.glob(os.path.join(read_dir,"*")):
     #Use convert("RGB") method for converting RGBA to RGB image.
     new_im = im.resize((600,400)).convert('RGB')
     basename = os.path.basename(file_name)
+    basename = basename.split(".tiff")[0]+".jpeg"
     print(basename)
     #Save them in the same path ~/supplier-data/images, with a JPEG extension.
     new_im.save(os.path.join(save_dir,basename),format='jpeg')
