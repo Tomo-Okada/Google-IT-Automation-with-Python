eb1393b2acab78160dee7a96f1c507d801c1a56e
Tomo <yokada@umich.edu>
2020-08-22 15:40:32 -0400

    Amend indent and import glob, os

--- week4/supplier_image_upload.py
+++ week4/supplier_image_upload.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import requests
+import glob, os
 
-read_dir = "/supplier-data/images"
+read_dir = "supplier-data/images"
 url = "http://localhost/upload/"
 
 for file_name in glob.glob(os.path.join(read_dir,"*.jpeg")):
     with open(file_name, 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+        r = requests.post(url, files={'file': opened})
