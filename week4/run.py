d60eb9c716a44f370cf350dfe55dc70775b37c13
Tomo <yokada@umich.edu>
2020-08-22 16:11:41 -0400

    import OrderedDict

--- week4/run.py
+++ week4/run.py
@@ -3,31 +3,31 @@
 import os
 import requests
 import locale
+from collections import OrderedDict
 
 # Path to the data
-path = "/supplier-data/descriptions/"
-url = "http://[linux-instance-external-IP]/fruits"
+path = "supplier-data/descriptions/"
+url = "http://107.178.210.28/fruits"
 keys = ["name", "weight", "description","image_name"]
-
-# TODO: change below
 
 folder = os.listdir(path)
 for file in folder:
     keycount = 0
-    fb = {}
+    fb = OrderedDict()
     with open(path + file) as fl:
         for line in fl:
             if keycount == 1:
                 # drop "lbs" and convert to an integer.
                 value = line.strip()
-                value = int(locale.atof(line.strip("lbs")))
+                value = line.strip(" lbs\n")
+                value = int(value)
             else:
                 value = line.strip()
             fb[keys[keycount]] = value
             keycount += 1
     # add all fields, including the image_name
-    value = os.path.basename(fl) + ".jpeg"
-    fb[keys[keycount+=1]] = value
+    name = file.split(".txt")[0]+".jpeg"
+    fb["image_name"] = name
 
     print(file,fb)
     response = requests.post(url, json=fb)
