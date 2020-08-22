40fb78985ca48c361f932844f3bbc28a03e82792
Tomo <yokada@umich.edu>
2020-08-22 14:07:09 -0400

    Integrated for Script to generate a PDF report

--- week4/report_email.py
+++ week4/report_email.py
@@ -3,8 +3,10 @@ import os, datetime, reports
 path = "/supplier-data/descriptions/"
 
 
-if __name__ == "__main__":
-    attachement = "/tmp/processed.pdf"
+def main(argv):
+    """Process the JSON data and generate a full report out of it."""
+    # creating the PDF report
+    attachment = "/tmp/processed.pdf"
     title = "Processed Update on <"+today.strftime("%B %d, %Y")+">"
 
     contents_list = []
@@ -14,3 +16,15 @@ if __name__ == "__main__":
     paragraph = "<br/>"+content["name"]+"<br/>"+content["weight"]+"<br/>"
 
     reports.generate_report(attachment, title, paragraph)
+
+    # send the email
+    sender = "automation@example.com"
+    receiver = "{}@example.com".format(os.environ.get('USER'))
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    message = emails.generate(sender, receiver, subject, body, attachment)
+    emails.send(message)
+
+
+if __name__ == "__main__":
+  main(sys.argv)
