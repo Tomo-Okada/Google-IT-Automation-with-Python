1c9ff05e3890b732f9d96cf6d3406f54ed31f4ce
Tomo <yokada@umich.edu>
2020-08-22 16:39:33 -0400

    Change to OrderedDict

--- week4/run.py
+++ week4/run.py
@@ -13,24 +13,31 @@ keys = ["name", "weight", "description","image_name"]
 folder = os.listdir(path)
 for file in folder:
     keycount = 0
+    # FIXME: dictionary does not retain order. It cause the error to upload JSON.
     fb = OrderedDict()
-    with open(path + file) as fl:
-        for line in fl:
-            if keycount == 1:
-                # drop "lbs" and convert to an integer.
-                value = line.strip()
-                value = line.strip(" lbs\n")
-                value = int(value)
-            else:
-                value = line.strip()
-            fb[keys[keycount]] = value
-            keycount += 1
-    # add all fields, including the image_name
     name = file.split(".txt")[0]+".jpeg"
-    fb["image_name"] = name
 
-    print(file,fb)
-    response = requests.post(url, json=fb)
+    with open(path + file) as fl:
+        fb["name"] = fl.readline().strip()
+        fb["weight"] = int(fl.readline().strip(" lbs\n"))
+        fb["description"] = fl.readline().strip()
+        # add all fields, including the image_name
+        fb["image_name"] = name
+
+        # for line in fl:
+        #     if keycount == 1:
+        #         # drop "lbs" and convert to an integer.
+        #         value = line.strip()
+        #         value = line.strip(" lbs\n")
+        #         value = int(value)
+        #     else:
+        #         value = line.strip()
+        #
+        #     fb[keys[keycount]] = value
+        #     keycount += 1
+    js = json.dumps(fb)
+    print(file,js)
+    response = requests.post(url, json=js)
 
 print("body: ", response.request.body)
 print("status: ",response.status_code)
