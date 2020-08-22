a2e1bd0dd8f1badc628ed9244f8f30456f18c67a
Tomo <yokada@umich.edu>
2020-08-22 11:07:06 -0400

    First ver for Working with supplier images

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+from PIL import Image
+import glob, os
 
-#Size: Change image resolution from 3000x2000 to 600x400 pixel
-#Format: Change image format from .TIFF to .JPEG
+read_dir = "/supplier-data/images"
+save_dir = "/supplier-data/images"
 
-# TODO: Use convert("RGB") method for converting RGBA to RGB image.
-
-
-# TODO: After processing the images, save them in the same path ~/supplier-data/images, with a JPEG extension.
+for file_name in glob.glob(os.path.join(read_dir,"*")):
+    im = Image.open(file_name)
+    #Size: Change image resolution from 3000x2000 to 600x400 pixel
+    #Format: Change image format from .TIFF to .JPEG
+    #Use convert("RGB") method for converting RGBA to RGB image.
+    new_im = im.resize((600,400)).convert('RGB')
+    basename = os.path.basename(file_name)
+    print(basename)
+    #Save them in the same path ~/supplier-data/images, with a JPEG extension.
+    new_im.save(os.path.join(save_dir,basename),format='jpeg')
